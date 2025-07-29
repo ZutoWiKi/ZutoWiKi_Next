cf6f18f27cf87c0dcd9c7433ba7461c4e88cb60f
DNSK <sslgh1024@naver.com>
2025-07-29 23:31:02 +0900

    ㅅㅂ

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,8 @@ export const metadata: Metadata = {
       default: "🌊 [모든 문학 경계의 파괴] - 문학 소통의 장, 윤슬",
       template: "%s | Yoonseul",
     },
-    description: "이거 왜이럼?",
+    description:
+      "햇빛이나 달빛이 바다나 파도에 비치어 반짝이는 잔물결인 윤슬처럼,\n같은 파도와 물결에 다양한 윤슬이 생길 수 있는 것처럼,\n같은 문학작품에서 당신만의 시선과 해석을 모두와 공유하세요!\n경계가 없는 파도처럼, 문학의 경계를 없애보세요!",
     images: "/yoonseul_logo.svg",
   },
 };
