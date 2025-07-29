35399994765a527128eebc40a60d580d67b4e2e2
DNSK <sslgh1024@naver.com>
2025-07-29 21:55:45 +0900

    로고, 설명 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,17 @@ import FloatingMenu from "@/components/FloatingMenu";
 export const metadata: Metadata = {
   title: "윤슬 | Yoonseul",
   description: "Developed by lgh, lhj",
+  metadataBase: new URL("https://yoonseul.site/"),
+  icons: {
+    icon: "/yoonseul_logo.svg",
+  },
+  openGraph: {
+    siteName: "yoonseul",
+    title: "yoonseul",
+    description: "문학 소통의 장",
+    images: "/yoonseul_logo.svg",
+    url: "https://yoonseul.site/",
+  },
 };
 
 export default function RootLayout({
