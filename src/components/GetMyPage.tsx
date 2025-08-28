1e9478aaa421cb2327c2f33612f3d7488494af1e
이형주 <jujumom630@gmail.com>
2025-08-28 23:37:19 +0900

    빌드에러...나는거에요?
    이겨.

--- src/components/GetMyPage.tsx
+++ src/components/GetMyPage.tsx
@@ -133,8 +133,39 @@ const GetMyPage: React.FC = () => {
                     {write.title}
                   </h3>
                   <div className="flex items-center gap-3 sm:gap-4 text-sm text-gray-500 sm:ml-4 sm:mt-1">
-                    <span className="flex items-center gap-1">️{write.views}</span>
-                    <span className="flex items-center gap-1">{write.likes}</span>
+                    <span className="flex items-center gap-1">️
+                      <svg
+                    className="w-4 h-4"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                    />
+                  </svg>
+                  {write.views}
+                  </span>
+                  
+                  <span className="flex items-center gap-1">
+                        <svg
+                      className="w-4 h-4"
+                      fill="none"
+                      stroke="currentColor"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                      />
+                    </svg>
+                    {write.likes}
+                    </span>
                   </div>
                 </div>
 
