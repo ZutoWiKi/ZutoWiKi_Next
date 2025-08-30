c80b135f000e9b943dbbf81b44c71d75503ee820
DNSK <sslgh1024@naver.com>
2025-08-30 21:57:27 +0900

    메인 페이지 모바일 UI 위치 조정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,17 +10,17 @@ export default function HomePage() {
     <PageLayout>
       <div className="max-w-7xl mx-auto px-4 sm:px-6 py-6 sm:py-12">
         <div className="flex flex-col lg:flex-row gap-6 lg:gap-10">
-          {/* 왼쪽 사이드바: 인기 작품 - 모바일에서는 상단에 위치 */}
-          <div className="w-full lg:w-[26%] order-2 lg:order-1">
-            <RecommendedWorks />
-          </div>
-
           {/* 오른쪽 메인 콘텐츠 */}
-          <main className="w-full lg:w-[74%] order-1 lg:order-2">
+          <main className="w-full lg:w-[74%] lg:order-2">
             <div className="space-y-6 sm:space-y-8">
               {/* 소개 섹션 */}
               <div className="text-center text-gray-600">
                 <PurposeSection />
+              </div>
+
+              {/* 모바일에서만 보이는 사이드바 - 소개와 전체 작품 사이에 위치 */}
+              <div className="block lg:hidden">
+                <RecommendedWorks />
               </div>
 
               {/* 전체 작품 가로 스크롤 섹션 */}
@@ -30,6 +30,11 @@ export default function HomePage() {
               <AllWritesSection />
             </div>
           </main>
+
+          {/* 왼쪽 사이드바: 인기 작품 - 데스크톱에서만 표시 */}
+          <div className="hidden lg:block lg:w-[26%] lg:order-1">
+            <RecommendedWorks />
+          </div>
         </div>
       </div>
     </PageLayout>
